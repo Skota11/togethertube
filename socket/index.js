596a8bcec5cf0d8490af873543ac428d9b57a261
S Kota <91359399+Skota11@users.noreply.github.com>
2023-06-06 21:49:34 +0900

    今までの苦労は全部無駄

--- socket/index.js
+++ socket/index.js
@@ -1,26 +1,9 @@
 const app = require("express")();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http);
-const cors = require('cors')
-
-const allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Content-Type, Authorization, access_token'
-  )
-
-  // intercept OPTIONS method
-  if ('OPTIONS' === req.method) {
-    res.send(200)
-  } else {
-    next()
-  }
-}
-app.use(allowCrossDomain)
-
-app.use(cors())
+const io = require("socket.io")(http , {cors: {
+  origin: "*",
+  methods: ["GET", "POST"]
+}});
 
 /**
  * [イベント] ユーザーが接続
