e7e0e022fffc94e676e68c735566df431ad5f04c
S Kota <91359399+Skota11@users.noreply.github.com>
2023-06-06 21:42:20 +0900

    NaNkanaorankaraIroIroAdd

--- socket/index.js
+++ socket/index.js
@@ -3,6 +3,23 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const cors = require('cors')
 
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, access_token'
+  )
+
+  // intercept OPTIONS method
+  if ('OPTIONS' === req.method) {
+    res.send(200)
+  } else {
+    next()
+  }
+}
+app.use(allowCrossDomain)
+
 app.use(cors())
 
 /**
